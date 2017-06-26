e096309a90c07b88f4a3334e4ff12bae54db5daa
doulighan <doulighan@gmail.com>
2017-06-26 17:25:48 -0400

    done

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -5,14 +5,14 @@ import ReactDOM from 'react-dom'
 export default class Product extends React.Component {
 
   render () {
-    return( 
-      <div className="product">
-         <h3>{this.props.name}</h3>
-         <p>{this.props.producer}</p>
-         <p>{this.props.hasWatermark}</p>
-         <p>{this.props.color}</p>
-         <p>{this.props.weight}</p>
-      </div>
+    return (
+        <div className="product">
+          <h3>{this.props.name}</h3>
+          <p>{this.props.producer}</p>
+          <p>{this.props.hasWatermark}</p>
+          <p>{this.props.color}</p>
+          <p>{this.props.weight}</p>
+        </div>
     )
   }
 }
@@ -29,8 +29,6 @@ for (var i = 80; i <= 300; i++) {
 
 Product.propTypes = {
   name: PropTypes.string.isRequired,
-  producer: PropTypes.string,
-  hasWatermark: PropTypes.bool,
   color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
   weight: PropTypes.oneOf(weightRange).isRequired 
 }
